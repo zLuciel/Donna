4857f0fe7ef3725e399aab0a876f1425dea6985d
zLuciel <82103687+zLuciel@users.noreply.github.com>
2024-02-23 06:21:57 -0500

    add

--- src/app/productos/page.js
+++ src/app/productos/page.js
@@ -1,18 +1,19 @@
-import FilterProducto from '@/components/filterProduct/FilterProducto'
-import Card from '@/components/slider/products/Card'
-import React from 'react'
-import categoriaMap from '@/components/slider/categoria/mapCategoria'
+import FilterProducto from "@/components/filterProduct/FilterProducto";
+import Card from "@/components/slider/products/Card";
+import React from "react";
+import categoriaMap from "@/components/slider/categoria/mapCategoria";
 const page = () => {
   return (
-    <div className='lg:container mx-auto grid grid-cols-[500px_minmax(900px,_1fr)_100px]'>
-     <FilterProducto/>
+    <div className="lg:container mx-auto grid grid-cols-[500px_minmax(900px,_1fr)_100px]">
+      <FilterProducto />
 
-     <div className=' grid-cols-4'>
-        
-     </div>
-
+      <div className=" grid grid-cols-4 gap-3">
+        {categoriaMap?.map((cate, i) => (
+          <Card key={i} image={cate.image} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ src/app/promociones/page.js
@@ -0,0 +1,55 @@
+"use client"
+import React from 'react'
+import { Carousel } from "@mantine/carousel";
+import { FaChevronRight,FaChevronLeft } from "react-icons/fa";
+import HomePortda from "@/components/slider/homePortada/HomePortda";
+import homeSlider from "@/data/homeSlider";
+import LoMejor from '@/components/slider/lomejor/Lomejor';
+import mejorSemana from '@/data/semana';
+import "@mantine/carousel/styles.css";
+const page = () => {
+  return (
+    <>
+    <main className="portada-main text-white h-screen w-screen">
+    <Carousel
+        className="portada"
+        slideSize="100%"
+        slideGap={{ base: 0 }}
+        loop
+        height="100%"
+        slidesToScroll={1}
+        nextControlIcon={<FaChevronRight className="text-3xl"/>}
+        previousControlIcon={<FaChevronLeft className="text-3xl"/>}
+      >
+        {homeSlider?.map((cate, i) => (
+          <Carousel.Slide key={i}>
+            <HomePortda image={cate.image} />
+          </Carousel.Slide>
+        ))}
+      </Carousel>
+    </main>
+    <section className="lg:container mx-auto   py-8  px-20">
+        <h2 className='text-4xl text-red-500 mb-10'>NUESTRAS PROMOCIONES</h2>
+        <Carousel
+         className="py-2"
+          withIndicators
+          slideSize="25%"
+          slideGap="md"
+          loop
+          height="100%"
+          slidesToScroll={4}
+          nextControlIcon={<FaChevronRight className="text-3xl"/>}
+          previousControlIcon={<FaChevronLeft className="text-3xl"/>}
+        >
+          {mejorSemana?.map((cate, i) => (
+            <Carousel.Slide key={i}>
+              <LoMejor image={cate.image} />
+            </Carousel.Slide>
+          ))}
+        </Carousel>
+      </section>
+    </>
+  )
+}
+
+export default page
