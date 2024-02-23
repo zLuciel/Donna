ec9069cd1e76bf94dd4e839dcdbf98bee12af1a1
Ness <82103687+zLuciel@users.noreply.github.com>
2024-02-23 13:49:15 -0500

    add

--- src/app/tiendas/page.js
+++ src/app/tiendas/page.js
@@ -30,7 +30,7 @@ const page = () => {
         </Carousel>
       </main>
       {/* mapa */}
-      <section className="lg:container px-20 py-10">
+      <section className="lg:container mx-auto px-20 py-10">
         <h2 className="text-3xl text-red-500 mb-5">BUSCA TU TIENDA MAS CERQUITA</h2>
         <div className="grid grid-cols-[250px_1fr] gap-8">
           <div className="flex flex-col gap-2">
