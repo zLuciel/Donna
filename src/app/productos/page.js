abe132e666ce74f45aa78d9f842d65a8839ba525
zLuciel <82103687+zLuciel@users.noreply.github.com>
2024-02-25 14:59:57 -0500

    add

--- src/app/productos/page.js
+++ src/app/productos/page.js
@@ -6,7 +6,8 @@ const page = () => {
   return (
     <div className="container px-8 mx-auto gap-3 py-10 grid grid-cols-[180px_1fr]">
       <FilterProducto />
-      <div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-3">
+      {/* grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-3 */}
+      <div className="auto-product-grid ">
         {categoriaMap?.map((cate, i) => (
           <Card key={i} image={cate.image} />
         ))}
