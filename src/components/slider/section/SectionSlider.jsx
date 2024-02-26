7e90c2d33837e313a86058074bbf3afcedd174eb
zLuciel <82103687+zLuciel@users.noreply.github.com>
2024-02-25 21:49:25 -0500

    add

--- src/components/slider/section/SectionSlider.jsx
+++ src/components/slider/section/SectionSlider.jsx
@@ -7,8 +7,8 @@ const SectionSlider = ({ renderjson, image , title,icon,pretIcoID,nextIcoID}) =>
     <section className="lg:container  mx-auto py-5  sm:px-20 md:px-20 lg:px-20 px-5" >
       <div className="flex justify-between pr-8">
         <span className="mb-5 text-xl font-extrabold flex items-center gap-3">
-          <p className="bg-red-600 text-white px-4 py-1 text-lg rounded-2xl">{icon}</p>
-          <h2 >
+          <p className="bg-red-600 text-white px-4 py-1 text-sm sm:text-sm md:text-lg lg:text-lg rounded-2xl">{icon}</p>
+          <h2 className="text-sm sm:text-base md:text-lg lg:text-lg">
           {title}
         </h2>
         </span>
