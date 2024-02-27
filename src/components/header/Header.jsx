3e044c8e027acbfe3489ecef7cc1dfa6476fb261
Ness <82103687+zLuciel@users.noreply.github.com>
2024-02-27 17:09:08 -0500

    add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ import { FaUser,FaShoppingCart } from "react-icons/fa";
 import Link from "next/link";
 import { IoIosArrowDown } from "react-icons/io";
 import ModalCategoria from "./ModalCategoria";
-
+import { FaClipboardList } from "react-icons/fa6";
 const Header = () => {
   const [view, setView] = useState(false);
   return (
@@ -30,20 +30,21 @@ const Header = () => {
       <div className="lg:container  mx-auto py-1 sm:px-10 md:px-10 lg:px-20 px-5  header-grid ">
       <Link href="/"><Image className="logo-header" src="/LogoDonna.png" width={90} height={90} alt="Donna" /></Link>
         <div className="categoria">
-         <button className="flex gap-2 justify-cent flex gap-2 items-center" onClick={()=> setView(true)}>Categorias <IoIosArrowDown /></button>
+         <button className="flex gap-2 text-sm justify-cent flex gap-2 items-center" onClick={()=> setView(true)}>Categorias <IoIosArrowDown /></button>
          <ModalCategoria view={view} setView={setView} />
         </div>
         <span className="search-header flex w-full  rounded-3xl overflow-hidden">
           <input
-            className="w-full outline-none px-3 py-1 "
+            className="w-full outline-none px-3 py-2 "
             type="text"
             placeholder="Dinos que buscas y te mostraremos las mejores ofertas"
           />
          <span className="bg-slate-gray px-4 flex justify-center items-center"><IoSearch /></span>
         </span>
-        <div className="cuenta-header flex gap-2 items-center ">
-          <span className="flex gap-2 justify-center items-center "><FaUser className="text-xl" /> <p className="prueba text-sm/[5px] mt-1">Mi cuenta</p></span>
-          <span className="flex gap-2 justify-center items-center"><FaShoppingCart className="text-xl"/> <p className="text-sm/[5px] mt-1">S/40.00</p></span>
+        <div className="cuenta-header flex gap-5 items-center ">
+        <span className="flex gap-2 flex-col justify-center items-center"><FaClipboardList className="text-xl" /> <p className="prueba text-sm mt-1 leading-3">Mi Lista</p></span>
+          <span className="flex gap-2 flex-col justify-center items-center "><FaUser className="text-xl" /> <p className="prueba text-sm mt-1 leading-3">Mi cuenta</p></span>
+          <span className="flex gap-2 flex-col justify-center items-center"><FaShoppingCart className="text-xl"/> <p className="text-sm mt-1 font-bold leading-3">S/40.00</p></span>
         </div>
       </div>
     </header>
