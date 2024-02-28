d42263816108d4a912567476ab4640476ff30a2f
Ness <82103687+zLuciel@users.noreply.github.com>
2024-02-28 15:21:11 -0500

    add

--- src/components/header/HeaderMovil.jsx
+++ src/components/header/HeaderMovil.jsx
@@ -8,16 +8,19 @@ import { IoIosArrowDown } from "react-icons/io";
 import { FaUser,FaShoppingCart } from "react-icons/fa";
 import { FaClipboardList } from "react-icons/fa6";
 import Search from "./Search";
+import Cart from "../svg/Cart";
+import List from "../svg/List";
+import User from "../svg/User";
 
 const HeaderMovil = ({ view, setView }) => {
 
   return (
-    <div className="lg:container  mx-auto py-1  px-5  header-movil ">
+    <div className="lg:container  mx-auto py-3  px-10  header-movil ">
       <div className="flex justify-between">
         {/* cuenta y categoria */}
         <div className="flex items-center gap-2">
           <span className="flex gap-2 flex-col justify-center items-center ">
-            <PiUserCircleLight className="text-3xl" />
+            <User className="text-3xl" />
           </span>
           <div className="categoria">
               <Burger size="sm"  onClick={() => setView(true)} aria-label="Toggle navigation" />
@@ -37,12 +40,20 @@ const HeaderMovil = ({ view, setView }) => {
         </Link>
         {/* lista y cart */}
         <div className="cuenta-header flex gap-5 items-center ">
-            <Search/>
-            <FaClipboardList className="text-xl" />{" "}
-            <FaShoppingCart />{" "}
+            <List className="text-xl" />{" "}
+            <Cart />
         </div>
-
+    
       </div>
+
+      <span className="search-header flex w-full  rounded-3xl overflow-hidden">
+          <input
+            className="w-full outline-none px-3 py-1 "
+            type="text"
+            placeholder="Dinos que buscas y te mostraremos las mejores ofertas"
+          />
+         <span className="px-4 flex justify-center items-center"><Search /></span>
+        </span>
     </div>
   );
 };
