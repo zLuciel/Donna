e52fbcb3241791e9404a542f5fae4bd8250e528c
Ness <82103687+zLuciel@users.noreply.github.com>
2024-03-11 17:44:52 -0500

    ADD

--- src/app/productos/actions.js
+++ src/app/productos/actions.js
@@ -1,7 +1,8 @@
 'use server'
 export async function dataProduct(id) {
     try {
-        const res = await fetch("http://localhost:3000/api/woocommerce/products?id="+id);
+        const URL = `${process.env.URL_WEB}/api/woocommerce/products?id=${id}`
+        const res = await fetch(URL);
         const response = await res.json()
         return await response.products
     } catch (e) {
