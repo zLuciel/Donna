d12a92322d1c27b4af7f119510dded5831b26f87
Ness <82103687+zLuciel@users.noreply.github.com>
2024-03-12 10:18:24 -0500

    add

--- src/app/productos/actions.js
+++ src/app/productos/actions.js
@@ -1,7 +1,7 @@
 'use server'
 export async function dataProduct(id) {
     try {
-        const URL = `${process.env.URL_WEB}/api/woocommerce/products?id=${id}`
+        const URL = `http://www.doona.com.pe/api/woocommerce/products?id=${id}`
         const res = await fetch(URL);
         const response = await res.json()
         return await response.products
