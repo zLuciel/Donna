c0552465e5a2d9475384c5b92c83f50f81c08b76
Ness <82103687+zLuciel@users.noreply.github.com>
2024-03-19 15:09:42 -0500

    add

--- src/hooks/useClickOutside.jsx
+++ src/hooks/useClickOutside.jsx
@@ -8,9 +8,9 @@ function useClickOutside(ref, callback) {
       }
     }
 
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('click', handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('click', handleClickOutside);
     };
   }, [ref, callback]);
 }
