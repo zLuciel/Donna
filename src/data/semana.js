e1d26df106564a883601b2aaae2e2472dfaaa67f
Ness <82103687+zLuciel@users.noreply.github.com>
2024-03-19 17:57:52 -0500

    add

--- src/data/semana.js
+++ src/data/semana.js
@@ -3,7 +3,7 @@ import Mejor1 from "@/assets/mejorSemana/mejor1.png"
 import Mejor2 from "@/assets/mejorSemana/mejor2.png"
 import Mejor3 from "@/assets/mejorSemana/mejor3.png"
 import Mejor4 from "@/assets/mejorSemana/mejor4.png"
-import ImageDefault from "@/assets/Image.Webp"
+import ImageDefault from "@/assets/Image.webp"
 const mejorSemana = [
     {
         image:ImageDefault,
