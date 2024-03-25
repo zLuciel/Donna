06d9be51bdf5adfdc7cd04b9321b77c38c7a9cf4
Ness <82103687+zLuciel@users.noreply.github.com>
2024-03-25 17:43:16 -0500

    add

--- src/components/lading/header/HeaderLanding.jsx
+++ src/components/lading/header/HeaderLanding.jsx
@@ -8,19 +8,17 @@ const HeaderLanding = () => {
     <header className="">
       <SubHeaderLading />
       <div className="md:container mx-auto flex justify-between items-center px-20 py-1">
-
-        <div className="flex justify-center items-center gap-6">
           <Image src={"/DonnaMovil.png"} alt="Donna" width={70} height={70} />
-        </div>
-       <div className="flex gap-5 items-center">
-       <ul className="flex gap-8 text-sm">
+        <div className="flex gap-5 items-center">
+          <ul className="flex gap-8 text-sm">
             <li>Productos</li>
             <li>Centro ayuda</li>
             <li>Clientes</li>
           </ul>
-          <Button variant="filled" color="#ff4d00">Whatsapp</Button>
-       </div>
-      
+          <Button variant="filled" color="#ff4d00">
+            Whatsapp
+          </Button>
+        </div>
       </div>
     </header>
   );
