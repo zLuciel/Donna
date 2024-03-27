34010ae0eb9e5fca03c7161c725a1b450d080ba5
Ness <82103687+zLuciel@users.noreply.github.com>
2024-03-27 17:21:43 -0500

    add

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,14 @@ import homeSlider from "@/data/homeSlider";
 import Image from "next/image";
 import React from "react";
 import Mockup from "@/assets/macku.png";
-import Beneficio from "@/assets/beneficio.png";
+import Beneficio from "@/assets/img_estar_bien.png";
 import CategoriaSlider from "@/components/lading/slider/CategoriaSlider";
 import Descuento from "@/components/lading/svg/Descuento";
 import UserSart from "@/components/lading/svg/UserSart";
 import Famili from "@/components/lading/svg/Famili";
 import { useProduct } from "./provider/ProviderContext";
 import Footer from "@/components/lading/footer/Footer";
-import gradient from "@/assets/img_gradient.png"
+import gradient from "@/assets/img_gradient.png";
 const Page = () => {
   const { sectionRefs } = useProduct();
   return (
@@ -32,7 +32,7 @@ const Page = () => {
         className="lg:container mx-auto px-20 py-10"
       >
         <h2 className="text-5xl roboto_bold mb-5 text-center">
-          Todo lo encuentras en <b className="text-gradient">Doona</b> 
+          Todo lo encuentras en <b className="text-[#ff4d00]">Doona</b>
         </h2>
         <CategoriaSlider />
       </section>
@@ -41,21 +41,21 @@ const Page = () => {
         ref={sectionRefs.tiendas}
         className="bg-[#E9E9E9] relative rounded-md lg:container  gap-10 mx-auto px-20 grid grid-cols-2"
       >
-      <Image
+        <Image
           alt="Mountains"
           src={gradient}
           fill
           sizes="(min-width: 808px) 50vw, 100vw"
           style={{
-            zIndex:"1",
-            objectFit: 'cover', // cover, contain, none
+            zIndex: "1",
+            objectFit: "cover", // cover, contain, none
           }}
         />
         <div className="flex flex-col gap-8 justify-self-center self-center">
-          <h2 className="text-5xl roboto_bold">
-            <b className="text-gradient">Hasta 60% dscto.</b> en tus marcas
+          <h3 className="text-5xl roboto_bold">
+            <b className="text-[#ff4d00] ">Hasta 60% dscto.</b> en tus marcas
             favoritas con tu App Doona
-          </h2>
+          </h3>
           <ul className="flex flex-col gap-5">
             <li className="flex gap-3 items-center text-sm">
               <Descuento />
@@ -73,7 +73,7 @@ const Page = () => {
             </li>
           </ul>
         </div>
-        <div style={{ zIndex:"2"}}>
+        <div style={{ zIndex: "2" }}>
           <Image src={Mockup} alt="mackup" width={600} height={600} />
         </div>
       </section>
@@ -81,33 +81,31 @@ const Page = () => {
       {/* Beneficios */}
       <section
         ref={sectionRefs.beneficio}
-        className="lg:container mx-auto px-20 py-10 gap-20 grid grid-cols-2"
+        className="lg:container mx-auto px-60 py-40"
       >
-        <div>
-          <Image src={Beneficio} alt="mackup" width={600} height={600} />
-        </div>
-
-        <div className="flex flex-col gap-8 justify-self-center self-center">
-          <h2 className="text-5xl roboto_bold">
-            <b className="text-gradient">Beneficio.</b> para una vida más fácil
-          </h2>
-          <ul className="flex flex-col gap-5">
-            <li className="flex gap-3 items-center text-sm">
-              <Descuento />
-              Accede a Clínica Internacional Digital y solicítalo para Medicina
-              General y Pediatría según tu plan.
-            </li>
-            <li className="flex gap-3 items-center text-sm">
-              <Famili />
-              Obtén un chequeo preventivo anual sin costo y mantén tu salud bajo
-              control.
-            </li>
-            <li className="flex gap-3 items-center text-sm">
-              <UserSart />
-              Recibe atención médica en casa y paga solo la consulta (Lima
-              Metropolitana y Callao).
-            </li>
-          </ul>
+        <div className="gradient-beneficio relative  items-center grid grid-cols-2  flex gap-4 px-10 py-20">
+          <div className="absolute container-image-beneficio">
+            <div className="relative ">
+              <Image
+                className="relative"
+                src={Beneficio}
+                alt="beneficio"
+                width={416}
+                height={416}
+              />
+            </div>
+          </div>
+          <div></div>
+          <div className="flex flex-col gap-3">
+            <h2 className="text-5xl font-bold roboto_Extrabol mb-3">
+              Beneficios para una vida más fácil
+            </h2>
+            <p className="font-semibold mb-5 text-md">
+              Y disfruta de talleres en vivo, clases cortas y sesiones
+              especializadas en psicología y nutrición.
+            </p>
+            <button className="button-beneficio">Conoce Estar Bien</button>
+          </div>
         </div>
       </section>
       <Footer />
--- src/components/lading/footer/Footer.jsx
+++ src/components/lading/footer/Footer.jsx
@@ -1,46 +1,31 @@
-import { useProduct } from "@/app/provider/ProviderContext";
 import Image from "next/image";
 import React from "react";
 import { BsInstagram } from "react-icons/bs";
-import { FaFacebook,FaTiktok} from "react-icons/fa";
+import { FaFacebook, FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
-  const { scrollToSection} = useProduct();
   return (
-    <footer className="bg-[#e9e9e9] p-3 flex  py-10 justify-center items-center text-sm">
-      <div className="lg:container justify-between px-40 flex gap-10  item-center">
-        {/* col-1 */}
-        <div className="flex gap-3 flex-col">
-          <Image
-            src="/DonnaMovil.png"
-            alt="Picture of the author"
-            width={120}
-            height={10}
-          />
-          <h3 className="roboto_bold mb-1">
-            ¿Más barato que el mercado?
-          </h3>
-          <p className="text-xs">© 2024 Mercado Doona</p>
-        </div>
-        {/* col -2 */}
-        <div className="flex flex-col gap-5">
-          <h3 className="text-md"><b>SECCIONES</b></h3>
-          <ul className="flex text-md flex-col gap-3">
-            <li className="text-[#3b3b3bef] cursor-pointer" onClick={() => scrollToSection('categorias')}>Producto</li>
-            <li className="text-[#3b3b3bef] cursor-pointer" onClick={() => scrollToSection('tiendas')}>Tienda</li>
-            <li className="text-[#3b3b3bef] cursor-pointer"conClick={() => scrollToSection('beneficio')}>Beneficios</li>
-          </ul>
-        </div>
-        {/* col -3 */}
-        <div className="flex flex-col gap-4">
-        <h3 className="text-md"><b>VISITA NUESTRAS REDES</b></h3>
-        <ul className="flex gap-5">
-          <li className="text-3xl text-[#3b3b3bef] "><BsInstagram /></li>
-          <li className="text-3xl text-[#3b3b3bef]"><FaFacebook /></li>
-          <li className="text-3xl text-[#3b3b3bef]"><FaTiktok /></li>
-        </ul>
-        </div>
+    <footer className="bg-[#e9e9e9] px-20 flex  py-1 justify-between items-center text-sm">
+      <div className="flex gap-4 items-center">
+        <Image
+          src="/DonnaMovil.png"
+          alt="Picture of the author"
+          width={70}
+          height={10}
+        />
+        <p className="text-xs">Copyright © 2024 Mercado Doona</p>
       </div>
+      <ul className="flex gap-5">
+        <li className="text-2xl text-[#3b3b3bef] ">
+          <BsInstagram />
+        </li>
+        <li className="text-2xl text-[#3b3b3bef]">
+          <FaFacebook />
+        </li>
+        <li className="text-2xl text-[#3b3b3bef]">
+          <FaTiktok />
+        </li>
+      </ul>
     </footer>
   );
 };
