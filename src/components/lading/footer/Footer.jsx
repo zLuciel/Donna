f341bccc394d9efd1efb52e21a66a5ce66d61dfe
Ness <82103687+zLuciel@users.noreply.github.com>
2024-03-27 15:16:43 -0500

    add

--- src/components/lading/footer/Footer.jsx
+++ src/components/lading/footer/Footer.jsx
@@ -1,9 +1,11 @@
+import { useProduct } from "@/app/provider/ProviderContext";
 import Image from "next/image";
 import React from "react";
 import { BsInstagram } from "react-icons/bs";
-import { FaFacebook,FaTiktok,FaLinkedin} from "react-icons/fa";
+import { FaFacebook,FaTiktok} from "react-icons/fa";
 
 const Footer = () => {
+  const { scrollToSection} = useProduct();
   return (
     <footer className="bg-[#e9e9e9] p-3 flex  py-10 justify-center items-center text-sm">
       <div className="lg:container justify-between px-40 flex gap-10  item-center">
@@ -24,9 +26,9 @@ const Footer = () => {
         <div className="flex flex-col gap-5">
           <h3 className="text-md"><b>SECCIONES</b></h3>
           <ul className="flex text-md flex-col gap-3">
-            <li className="text-[#3b3b3bef]">Producto</li>
-            <li className="text-[#3b3b3bef]">Tienda</li>
-            <li className="text-[#3b3b3bef]">Beneficios</li>
+            <li className="text-[#3b3b3bef] cursor-pointer" onClick={() => scrollToSection('categorias')}>Producto</li>
+            <li className="text-[#3b3b3bef] cursor-pointer" onClick={() => scrollToSection('tiendas')}>Tienda</li>
+            <li className="text-[#3b3b3bef] cursor-pointer"conClick={() => scrollToSection('beneficio')}>Beneficios</li>
           </ul>
         </div>
         {/* col -3 */}
