5a2d9cad3e1f3ee28e2661aeb9fa24a0fc868968
Ness <82103687+zLuciel@users.noreply.github.com>
2024-04-08 17:31:23 -0500

    add

--- src/data/homeSlider.js
+++ src/data/homeSlider.js
@@ -1,7 +1,7 @@
 import Portada20 from "@/assets/BANNER-DOONA-1.webp"
 import Portada1 from "@/assets/BANNER-PRINCIPAL-DOONA.webp"
 import Portada2 from "@/assets/BANNER-PRINCIPAL-DOONA-2.webp"
-import Portada3 from "@/assets/portada/portada3.jpg"
+import Portada3 from "@/assets/BANNER-PRINCIPAL-CELULAR.webp"
 import Portada4 from "@/assets/portada/portada4.jpg"
 import Portada5 from "@/assets/portada/portada5.jpg"
 import Portada6 from "@/assets/portada/portada6.jpg"
@@ -16,7 +16,7 @@ const homeSlider = [
         name:"Carne",
     },
     {
-        image:Portada1,
+        image:Portada3,
         name:"Carne",
     },
     {
