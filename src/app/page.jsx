6c340a568aa2d4e4457f74098a19da69b14686a0
Ness <82103687+zLuciel@users.noreply.github.com>
2024-04-12 14:49:34 -0500

    cambios

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -38,7 +38,7 @@ const Page = () => {
         <div className="title-div bg-[#FF4D00]"></div>
         <div className="lg:container py-4 lg:px-20 px-6 title-categoria mx-auto">
           <h2 className="h2-formate-white  lg:text-5xl sm:text-3xl text-3xl xs:text-3xl md:text-4xl title-nunito ">
-            Nuestras categorias
+            Nuestras categorías
           </h2>
         </div>
         <CategoriaSlider />
--- src/components/lading/main/Main.jsx
+++ src/components/lading/main/Main.jsx
@@ -3,29 +3,46 @@ import Image from "next/image";
 import Verdura from "@/assets/VERDURAS.png";
 import Wave from "@/assets/wave.png";
 import WaveTop from "@/assets/wave-top.png";
+import Link from "next/link";
 const Main = () => {
   return (
     <main className="portada-main py-20 w-screen relative">
-      <div className="absolute bottom-0 w-full" style={{height:"300px"}}>
-      <Image src={Wave} sizes="100vw" fill alt="wave"/>
+      <div className="absolute bottom-0 w-full" style={{ height: "300px" }}>
+        <Image src={Wave} sizes="100vw" fill alt="wave" />
       </div>
-      <div className="absolute top-0 w-full" style={{height:"200px"}}>
-      <Image src={WaveTop} sizes="100vw" fill alt="wave"/>
+      <div className="absolute top-0 w-full" style={{ height: "200px" }}>
+        <Image src={WaveTop} sizes="100vw" fill alt="wave" />
       </div>
       <div className="xl:container  lg:px-20 mx-auto  grid main-grid place-self-center items-center h-portada ">
         {/* col 1 */}
         <div className="flex flex-col gap-8 z-[1] ">
           <h1 className="text-8xl title-nunito text-[#ff4d00] ">
-          ! TODO LO QUE NECESITAS ¡
+            ! TODO LO QUE NECESITAS ¡
           </h1>
-          <p className="nunito-sub-title sub-title-main text-2xl">Delivery gratis a tu casa</p>
-         <div className="flex gap-7">
-          <button type="button" className="text-md btn-default btn-main text-white bg-[#ff4d00] font-semibold">Quiero conocer màs</button>
-          <button type="button" style={{border:"1px solid #696969"}} className="btn-main text-md btn-default font-semibold">Quiero conocer màs</button>
-         </div>
+          <p className="nunito-sub-title sub-title-main text-2xl">
+            Delivery gratis a tu casa
+          </p>
+          <div className="flex gap-7">
+            <Link
+              href="https://wa.link/njs0op"
+              target="_blank"
+              className="text-md btn-default btn-main text-white bg-[#ff4d00] font-semibold"
+            >
+              Quiero conocer màs
+            </Link>
+            <Link
+              href="#"
+              target="_blank"
+              type="button"
+              style={{ border: "1px solid #696969" }}
+              className="btn-main text-md btn-default font-semibold"
+            >
+              Ver productos
+            </Link>
+          </div>
         </div>
         {/* col 2 */}
-        <div style={{ position: 'relative', height: '100%', width:"100%"}}>
+        <div style={{ position: "relative", height: "100%", width: "100%" }}>
           <Image
             src={Verdura}
             className="portada"
