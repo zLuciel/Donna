52f7fc1a456e0e8f45c474f0944ee2eb831380b2
Ness <82103687+zLuciel@users.noreply.github.com>
2024-04-16 14:48:52 -0500

    cambios

--- src/app/productos/page.js
+++ src/app/productos/page.js
@@ -4,8 +4,8 @@ import PageProduct from "@/components/lading/PageProduct/PageProduct"
 
 const page= ({searchParams}) => {
   // get parametros
-  const $category = searchParams.get("categoria");
-  const $mostrar = searchParams.get("mostrar")
+  const $category = searchParams.categoria;
+  const $mostrar = searchParams.mostrar;
   return (
     <PageProduct  $category={$category} $mostrar={$mostrar} />
 
