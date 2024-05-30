0ffa8b17379d3b061af42774ce9291565aa867e2
Ness <82103687+zLuciel@users.noreply.github.com>
2024-05-30 10:12:36 -0500

    add

--- src/components/lading/CardSkeleton/CardSkeleton.jsx
+++ src/components/lading/CardSkeleton/CardSkeleton.jsx
@@ -4,7 +4,7 @@ const CardSkeleton = () => {
   return (
     <div>
     <Skeleton height={50} mb="xl" />
-    <Skeleton height={8} radius="xl" />
+    <Skeleton height={7} radius="xl" />
     <Skeleton height={8} mt={6} radius="xl" />
     <Skeleton height={8} mt={6} width="70%" radius="xl" />
     <Skeleton height={8} mt={6} width="70%" radius="xl" />
